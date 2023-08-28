4bae1602c9386609a4e8d10a7fa9a6860609b5ad
Juan Carlos <jcarlosriego@gmail.com>
2023-08-28 18:20:46 +0200

    ListView.test updated

--- src/test/ListView.test.js
+++ src/test/ListView.test.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ListView from '../components/ListView';
+import Item from '../components/Item';
 
-const mockItems = [
-  { brand: "Apple", model: "iPhone 13", price: 999, imgUrl: "some-url" },
-  { brand: "Samsung", model: "Galaxy S22", price: 899, imgUrl: "some-url" },
-];
+jest.mock('../components/Item', () => () => <div data-testid="mock-item"></div>);
 
-describe('ListView', () => {
-  it('renders without crashing', () => {
-    const { getByTestId } = render(<ListView items={mockItems} />);
-    expect(getByTestId('list-view')).toBeInTheDocument();
-  });
+describe('ListView Component', () => {
+  const mockItems = [
+    { id: '1', brand: 'Apple', model: 'iPhone 15', price: '999', imgUrl: 'https://example.com/iphone.jpg' },
+    { id: '2', brand: 'Samsung', model: 'Galaxy S23', price: '899', imgUrl: 'https://example.com/galaxy.jpg' },
+  ];
 
-  it('renders the correct number of items', () => {
-    const { getAllByTestId } = render(<ListView items={mockItems} />);
-    const items = getAllByTestId(/^list-item-/);
-    expect(items.length).toBe(mockItems.length);
+  it('renders correctly', () => {
+    render(<ListView items={mockItems} />);
+    
+    const listView = screen.getByTestId('list-view');
+    expect(listView).toBeInTheDocument();
+    
+    const listItems = screen.getAllByTestId(/list-item-/);
+    expect(listItems).toHaveLength(2);
+
+    const mockItemComponents = screen.getAllByTestId('mock-item');
+    expect(mockItemComponents).toHaveLength(2);
   });
 });
