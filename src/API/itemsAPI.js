b7e37a02797f3505a0067ec3207afb93c3c434e9
Juan Carlos <jcarlosriego@gmail.com>
2023-08-28 14:44:33 +0200

    Configured cache

--- src/API/itemsAPI.js
+++ src/API/itemsAPI.js
@@ -1,29 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
+import { getCache, setCache } from "../utils/cacheConfig";
 
-const baseUrl = 'https://itx-frontend-test.onrender.com/api';
+const baseUrl = "https://itx-frontend-test.onrender.com/api";
 
 export const fetchItems = () => {
-  return axios.get(`${baseUrl}/product`)
-    .then(response => response.data)
-    .catch(error => {
+  const cachedData = getCache("items");
+  if (cachedData) {
+    return Promise.resolve(cachedData);
+  }
+
+  return axios
+    .get(`${baseUrl}/product`)
+    .then((response) => {
+      setCache("items", response.data);
+      return response.data;
+    })
+    .catch((error) => {
       throw error;
     });
 };
 
 export const fetchItemDetail = (id) => {
-  return axios.get(`${baseUrl}/product/${id}`)
-    .then(response => response.data)
-    .catch(error => {
+  const cachedData = getCache(`item_${id}`);
+  if (cachedData) {
+    return Promise.resolve(cachedData);
+  }
+
+  return axios
+    .get(`${baseUrl}/product/${id}`)
+    .then((response) => {
+      setCache(`item_${id}`, response.data);
+      return response.data;
+    })
+    .catch((error) => {
       throw error;
     });
 };
 
-export const submitItemtoCart = ( itemDetails ) => {
-  return axios.post(`${baseUrl}/cart`, itemDetails)
-    .then(response => {
+export const submitItemtoCart = (itemDetails) => {
+  return axios
+    .post(`${baseUrl}/cart`, itemDetails)
+    .then((response) => {
       return response.data.count;
     })
-    .catch(error => {
-      console.error('Error adding item to chart', error);
+    .catch((error) => {
+      console.error("Error adding item to chart", error);
     });
-}
+};
